c17922a05a3a646dc2c02d0d43ee6e2b1b3c3547
Alessandro <alessandrovaru@gmail.com>
2024-05-16 14:45:49 -0400

    chore: Update next.config.mjs to include sassOptions for styles directory
    Remove unused code and update next.config.mjs to include sassOptions for including paths to the styles directory.

--- src/components/home/Hero/Hero.jsx
+++ src/components/home/Hero/Hero.jsx
@@ -1,18 +1,11 @@
+import styles from './Hero.module.sass'
+
+
 export const Hero = () => {
   return (
-    <div className="hero">
-      <div className="hero__content">
-        <h1 className="hero__title">Welcome to the world of tomorrow!</h1>
-        <p className="hero__text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          condimentum, nisl in lacinia vehicula, libero nunc ultrices nisi, nec
-          dictum odio felis a nunc. Sed nec purus auctor, ultricies mi nec,
-          ultrices nunc. Integer nec elit in nulla ultricies tincidunt. Nullam
-          nec purus auctor, ultricies mi nec, ultrices nunc. Integer nec elit in
-          nulla ultricies tincidunt.
-        </p>
-        <button className="hero__button">Learn more</button>
-      </div>
-    </div>
+    <section className={styles.Hero}>
+      <h1>Future world</h1>
+      <h2>Empowering Your Tomorrow, Today!</h2>
+    </section>
   );
 }
