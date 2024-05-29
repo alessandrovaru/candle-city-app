bbdfa58cc43b10308af2732dd6e227242e3247fd
Alessandro <alessandrovaru@gmail.com>
2024-05-28 23:57:23 -0400

    refactor: Update MainProducts component to use next/image for lazy loading
    Refactor the MainProducts component to use the next/image package for lazy loading of images. This improves the page load performance by loading images only when they are in the viewport. The lazy loading functionality is implemented using the Image component from next/image, providing a better user experience.

--- src/components/home/MainProducts/MainProducts.jsx
+++ src/components/home/MainProducts/MainProducts.jsx
@@ -1,57 +1,37 @@
-import Image from "next/image";
+import Image from 'next/image'
+import styles from './MainProducts.module.sass'
 
-import styles from "./MainProducts.module.sass";
-
-
-const getProduct = async () => {
+const getProducts = async () => {
   try {
     const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`, {
-      headers: {
-        'X-Shopify-Access-Token': process.env.SHOPIFY_ACCESS_TOKEN,
-      },
-    });
-    throw new Error('Error fetching products');
-    const { products } = await response.json();
-    return products;
+      headers: new Headers({
+        'X-Shopify-Access-Token': process.env.SHOPIFY_ACCESS_TOKEN || ""
+      })
+    })
+    const { products } = await response.json()
+    return products
   } catch (error) {
-    console.error('Error fetching products:', error);
-    return null;
+    console.log(error)
   }
-};
+}
 
 export const MainProducts = async () => {
-
-  const products = await getProduct();
-  
+  const products = await getProducts()
   
   return (
-    <div className="main-products">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <div className="main-products__content">
-            {products?.map((product) => {
-                const imageSrc = product.images[0].src;
-                return (
-                  <div key={product.id}>
-                    <h2>{product.title}</h2>
-                    <div className={styles.imageContainer}>
-                      <Image src={imageSrc} fill alt={product.title} loading="eager" />
-                    </div>
-                    <button>Shop Now</button>
-                  </div>
-                );
-              }
-            )}
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className="main-products__img">
-              image
-            </div>
-          </div>
-        </div>
+    <section className={styles.MainProducts}>
+      <h3>✨ New products released!</h3>
+      <div className={styles.MainProducts__grid}>
+        {products?.map((product) => {
+          const imageSrc = product.images[0].src;
+          return (
+            <article key={product.id}>
+              <p>{product.title}</p>
+              <Image src={imageSrc} fill alt={product.title} loading="eager" />
+            </article>
+          )
+        })}
       </div>
-    </div>
-  );
+    </section>
+  )
 }
--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -1,7 +1,28 @@
+import Link from 'next/link'
+import styles from './Header.module.css'
+
 export const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
-  );
+    <header>
+      <nav>
+        <ul className={styles.Header__list}>
+          <li>
+            <Link href="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/portfolio">
+              Portfolio
+            </Link>
+          </li>
+          <li>
+            <Link href="/test">
+              Test
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    )
 }
